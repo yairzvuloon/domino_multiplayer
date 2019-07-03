fd76babbd4e34492c35794838258aad4802efb7b
Yair Zvuloon <yairzv0@gmail.com>
2019-07-03 17:16:04 +0300

    init repo

---
+++ src/components/baseContainer.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginModal from './login-modal.jsx';
+import ChatContaier from './chatContainer.jsx';
+
+export default class BaseContainer extends React.Component {
+    constructor(args) {
+        super(...args);
+        this.state = {
+            showLogin: true,
+            currentUser: {
+                name: ''
+            }
+        };
+        
+        this.handleSuccessedLogin = this.handleSuccessedLogin.bind(this);
+        this.handleLoginError = this.handleLoginError.bind(this);
+        this.fetchUserInfo = this.fetchUserInfo.bind(this);
+        this.logoutHandler= this.logoutHandler.bind(this);
+
+        this.getUserName();
+    }
+    
+    render() {        
+        if (this.state.showLogin) {
+            return (<LoginModal loginSuccessHandler={this.handleSuccessedLogin} loginErrorHandler={this.handleLoginError}/>)
+        }
+        return this.renderChatRoom();
+    }
+
+
+    handleSuccessedLogin() {
+        this.setState(()=>({showLogin:false}), this.getUserName);        
+    }
+
+    handleLoginError() {
+        console.error('login failed');
+        this.setState(()=>({showLogin:true}));
+    }
+
+    renderChatRoom() {
+        return(
+            <div className="chat-base-container">
+                <div className="user-info-area">
+                    Hello {this.state.currentUser.name}
+                    <button className="logout btn" onClick={this.logoutHandler}>Logout</button>
+                </div>
+                <ChatContaier />                
+            </div>
+        )
+    }
+
+    getUserName() {
+        this.fetchUserInfo()
+        .then(userInfo => {
+            this.setState(()=>({currentUser:userInfo, showLogin: false}));
+        })
+        .catch(err=>{            
+            if (err.status === 401) { // incase we're getting 'unautorithed' as response
+                this.setState(()=>({showLogin: true}));
+            } else {
+                throw err; // in case we're getting an error
+            }
+        });
+    }
+
+    fetchUserInfo() {        
+        return fetch('/users',{method: 'GET', credentials: 'include'})
+        .then(response => {            
+            if (!response.ok){
+                throw response;
+            }
+            return response.json();
+        });
+    }
+
+    logoutHandler() {
+        fetch('/users/logout', {method: 'GET', credentials: 'include'})
+        .then(response => {
+            if (!response.ok) {
+                console.log(`failed to logout user ${this.state.currentUser.name} `, response);                
+            }
+            this.setState(()=>({currentUser: {name:''}, showLogin: true}));
+        })
+    }
+}
